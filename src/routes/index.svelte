f8f13777ad79b0f972bbd14f69003d239ba2ebba
Evan Winter <evanwinter@Evans-MacBook-Pro.local>
2021-10-10 22:05:43 -0500

    Small copy update and update dependencies

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<h1>Active Link Styles Example</h1>
+<h1>SvelteKit Active Link Styles Example</h1>
 <p>
   Navigate between pages using the links above, and the link representing the
   current page will have additional styles.
