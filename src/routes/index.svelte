99e52ec996f941790c7ec2eac836dd3a37e23c85
Evan Winter <evanwinter@Evans-MacBook-Pro.local>
2021-10-10 21:53:37 -0500

    Update home page copy

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Active Link Styles Example</h1>
+<p>
+  Navigate between pages using the links above, and the link representing the
+  current page will have additional styles.
+</p>
